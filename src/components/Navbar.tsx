a5e32581cd17cf2f962b560fba697c149000a829
joselisboaa <joselisboaa05@gmail.com>
2025-02-18 20:11:08 -0300

    feat: change theme color and add index path

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,12 @@
 "use client";
 
 import React, { useState } from "react";
-import { AppBar, Toolbar, Typography, Button, Box, Drawer, List, ListItem, ListItemText, IconButton } from "@mui/material";
-import { Menu as MenuIcon, ExitToApp as ExitIcon } from "@mui/icons-material";
+import { AppBar, Toolbar, Typography, Button, Box, Drawer, List, ListItem, ListItemText, IconButton, Divider } from "@mui/material";
+import { Menu as MenuIcon, ExitToApp as ExitIcon, Group, Help, QuestionAnswer, Info } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
+import HomeIcon from '@mui/icons-material/Home';
 import Cookies from "js-cookie";
+import Home from "@/app/home/page";
 
 export default function Navbar() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
@@ -27,75 +29,83 @@ export default function Navbar() {
 
   return (
     <>
-      <AppBar position="sticky">
+      <AppBar position="sticky" sx={{ backgroundColor: "#7E57C2" }}>
         <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            onClick={() => toggleDrawer(true)}
-            sx={{ mr: 2 }}
-          >
+          <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => toggleDrawer(true)} sx={{ mr: 2 }}>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
             Mapa de Acessibilidade
           </Typography>
-          <Button color="inherit" onClick={() => navigateTo("/home/user-group")}>
-            Grupos
-          </Button>
-          <Button color="inherit" onClick={() => navigateTo("/home/questions")}>
-            Questões
-          </Button>
-          <Button color="inherit" onClick={() => navigateTo("/home/answers")}>
-            Respostas
-          </Button>
-          <Button color="inherit" onClick={() => navigateTo("/home/orientations")}>
-            Orientações
-          </Button>
-          <Button
-            color="inherit"
-            onClick={handleLogout}
-            startIcon={<ExitIcon />}
-            sx={{
-              marginLeft: 2,
-              fontWeight: "bold",
-              color: "inherit",
-              border: "2px solid",
-              borderColor: "inherit",
-              "&:hover": {
-                backgroundColor: "rgba(0, 0, 0, 0.08)",
-              },
-            }}
-          >
-            Sair
-          </Button>
+          <Box>
+            <Button color="inherit" startIcon={<HomeIcon />} onClick={() => navigateTo("/home")}>
+              Inicio
+            </Button>
+            <Button color="inherit" startIcon={<Group />} onClick={() => navigateTo("/home/user-group")}>
+              Grupos
+            </Button>
+            <Button color="inherit" startIcon={<Help />} onClick={() => navigateTo("/home/questions")}>
+              Questões
+            </Button>
+            <Button color="inherit" startIcon={<QuestionAnswer />} onClick={() => navigateTo("/home/answers")}>
+              Respostas
+            </Button>
+            <Button color="inherit" startIcon={<Info />} onClick={() => navigateTo("/home/orientations")}>
+              Orientações
+            </Button>
+            <Button
+              color="inherit"
+              onClick={handleLogout}
+              startIcon={<ExitIcon />}
+              sx={{
+                marginLeft: 2,
+                fontWeight: "bold",
+                color: "inherit",
+                border: "2px solid",
+                borderColor: "inherit",
+                "&:hover": {
+                  backgroundColor: "rgba(0, 0, 0, 0.08)",
+                },
+              }}
+            >
+              Sair
+            </Button>
+          </Box>
         </Toolbar>
       </AppBar>
 
       <Drawer anchor="left" open={isDrawerOpen} onClose={() => toggleDrawer(false)}>
-        <List
-          sx={{ width: 250 }}
-          role="presentation"
-          onClick={() => toggleDrawer(false)}
-          onKeyDown={() => toggleDrawer(false)}
-        >
-          <ListItem component="button" onClick={() => navigateTo("/home/user-group")} sx={{ cursor: "pointer" }}>
-            <ListItemText primary="Grupos" />
-          </ListItem>
-          <ListItem component="button" onClick={() => navigateTo("/home/questions")} sx={{ cursor: "pointer" }}>
-            <ListItemText primary="Questões" />
-          </ListItem>
-          <ListItem component="button" onClick={() => navigateTo("/home/answers")} sx={{ cursor: "pointer" }}>
-            <ListItemText primary="Respostas" />
-          </ListItem>
-          <ListItem component="button" onClick={() => navigateTo("/home/orientations")} sx={{ cursor: "pointer" }}>
-            <ListItemText primary="Orientações" />
-          </ListItem>
-          <ListItem component="button" onClick={handleLogout} sx={{ cursor: "pointer" }}>
-            <ListItemText primary="Sair" />
-          </ListItem>
-        </List>
+        <Box sx={{ width: 250 }} role="presentation" onClick={() => toggleDrawer(false)} onKeyDown={() => toggleDrawer(false)}>
+          <List>
+            <ListItem component="button" onClick={() => navigateTo("/home")}>
+              <HomeIcon sx={{ mr: 1 }} />
+              <ListItemText primary="Início" />
+            </ListItem>
+            <ListItem component="button" onClick={() => navigateTo("/home/user-group")}>
+              <Group sx={{ mr: 1 }} />
+              <ListItemText primary="Grupos" />
+            </ListItem>
+            <ListItem component="button" onClick={() => navigateTo("/home/questions")}>
+              <Help sx={{ mr: 1 }} />
+              <ListItemText primary="Questões" />
+            </ListItem>
+            <ListItem component="button" onClick={() => navigateTo("/home/answers")}>
+              <QuestionAnswer sx={{ mr: 1 }} />
+              <ListItemText primary="Respostas" />
+            </ListItem>
+            <ListItem component="button" onClick={() => navigateTo("/home/orientations")}>
+              <Info sx={{ mr: 1 }} />
+              <ListItemText primary="Orientações" />
+            </ListItem>
+          </List>
+          <Divider />
+          <List>
+            <ListItem component="button" onClick={handleLogout}>
+              <ExitIcon sx={{ mr: 1 }} />
+              <ListItemText primary="Sair" />
+            </ListItem>
+          </List>
+        </Box>
       </Drawer>
     </>
   );
