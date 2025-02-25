2acab427ffb3d96a0fcf3c393a8b9ae20089193c
joselisboaa <joselisboaa05@gmail.com>
2025-02-24 23:28:46 -0300

    feat: add react-hook-form to validate the form on CreateAnswer

--- src/app/home/answers/new/page.tsx
+++ src/app/home/answers/new/page.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { Box, Button, TextField, Typography, CircularProgress, Autocomplete, Paper, FormControlLabel, Switch } from "@mui/material";
 import { useSnackbar } from "notistack";
-import { useQuery } from "react-query";
+import { useQuery, useMutation } from "react-query";
+import { useForm, Controller } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import fetchRequest from "@/utils/fetchRequest";
 
 interface Question {
@@ -13,80 +15,66 @@ interface Question {
   text: string;
 }
 
+const schema = yup.object().shape({
+  text: yup.string().trim().required("O texto da resposta é obrigatório"),
+  question: yup
+    .object()
+    .shape({ id: yup.number().required("A questão é obrigatório"), text: yup.string().required("A questão é obrigatório") })
+    .test("valid-question", "A questão é obrigatório", (value) => {
+      return value && value.id !== 0 && value.text.trim() !== "";
+    })
+    .nullable()
+    .required("A pergunta é obrigatória"),
+  other: yup.boolean().required("O campo outros é obrigatório"),
+});
+
 export default function CreateAnswer() {
-  const [text, setText] = useState("");
-  const [selectedQuestion, setSelectedQuestion] = useState<Question | null>(null);
-  const [other, setOther] = useState(false);
-  const [loading, setLoading] = useState(false);
   const router = useRouter();
   const { enqueueSnackbar } = useSnackbar();
+  
+  const { control, handleSubmit, formState: { errors }, } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      text: "",
+      question: { id: 0, text: "" },
+      other: false,
+    },
+  });
 
-  const { data: questions, isLoading: isFetchingQuestions } = useQuery(
+  const { data: questions, isLoading: isFetchingQuestions } = useQuery<Question[]>(
     "questions",
     async () => {
-      const response = await fetchRequest<null, Question[]>("/questions", {
-        method: "GET",
-      });
+      const response = await fetchRequest<null, Question[]>("/questions", { method: "GET" });
       return response.body;
     },
     {
-      onError: (error: unknown) => {
-        enqueueSnackbar(
-          `Erro ao carregar perguntas: ${error instanceof Error ? error.message : "Erro desconhecido"}`,
-          { variant: "error" }
-        );
+      onError: (error) => {
+        enqueueSnackbar(`Erro ao carregar perguntas: ${error instanceof Error ? error.message : "Erro desconhecido"}`, { variant: "error" });
       },
     }
   );
 
-  const handleSubmit = async () => {
-    if (!text.trim() || !selectedQuestion) {
-      enqueueSnackbar("Preencha todos os campos", { variant: "warning" });
-      return;
-    }
-
-    try {
-      setLoading(true);
+  const createMutation = useMutation(
+    async (data: any) => {
       await fetchRequest("/answers", {
         method: "POST",
-        body: { text, question_id: selectedQuestion.id, other },
+        body: { text: data.text, question_id: data.question.id, other: data.other },
       });
-
-      enqueueSnackbar("Resposta criada com sucesso!", { variant: "success" });
-      router.push("/home/answers");
-    } catch (error) {
-      enqueueSnackbar(
-        `Erro ao criar resposta: ${error instanceof Error ? error.message : "Erro desconhecido"}`,
-        { variant: "error" }
-      );
-    } finally {
-      setLoading(false);
+    },
+    {
+      onSuccess: () => {
+        enqueueSnackbar("Resposta criada com sucesso!", { variant: "success" });
+        router.push("/home/answers");
+      },
+      onError: (error) => {
+        enqueueSnackbar(`Erro ao criar resposta: ${error instanceof Error ? error.message : "Erro desconhecido"}`, { variant: "error" });
+      },
     }
-  };
-
-  const handleCancel = () => {
-    router.push("/home/answers");
-  };
+  );
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "fit",
-      }}
-    >
-      <Paper
-        elevation={4}
-        sx={{
-          padding: 4,
-          width: "100%",
-          maxWidth: 420,
-          borderRadius: 3,
-          textAlign: "center",
-        }}
-      >
+    <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "fit" }}>
+      <Paper elevation={4} sx={{ padding: 4, width: "100%", maxWidth: 420, borderRadius: 3, textAlign: "center" }}>
         <Typography variant="h4" component="h1" gutterBottom sx={{ color: "#5E3BEE" }}>
           Criar Resposta
         </Typography>
@@ -94,80 +82,76 @@ export default function CreateAnswer() {
           Preencha os detalhes da resposta abaixo.
         </Typography>
 
-        <Box sx={{ display: "grid", gap: 2 }}>
-          <TextField
-            label="Texto da Resposta"
-            fullWidth
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            variant="outlined"
-          />
+        <form onSubmit={handleSubmit((data) => createMutation.mutate(data))}>
+          <Box sx={{ display: "grid", gap: 2 }}>
+            <Controller
+              name="text"
+              control={control}
+              render={({ field }) => <TextField {...field} label="Texto da Resposta" fullWidth variant="outlined" error={!!errors.text} helperText={errors.text?.message}/>}
+            />
 
-          <Autocomplete
-            value={selectedQuestion}
-            onChange={(event, newValue) => setSelectedQuestion(newValue)}
-            options={questions || []}
-            getOptionLabel={(option) => option.text}
-            isOptionEqualToValue={(option, value) => option.id === value.id}
-            loading={isFetchingQuestions}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Selecionar Pergunta"
-                fullWidth
-                required
-                InputProps={{
-                  ...params.InputProps,
-                  endAdornment: (
-                    <>
-                      {isFetchingQuestions ? <CircularProgress size={20} /> : null}
-                      {params.InputProps.endAdornment}
-                    </>
-                  ),
-                }}
-              />
-            )}
-          />
+            <Controller
+              name="question"
+              control={control}
+              render={({ field }) => (
+                <Autocomplete
+                  {...field}
+                  options={questions || []}
+                  getOptionLabel={(option) => option.text}
+                  isOptionEqualToValue={(option, value) => option.id === value.id}
+                  onChange={(_, newValue) => field.onChange(newValue)}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Selecionar Pergunta"
+                      fullWidth
+                      variant="outlined"
+                      error={!!errors.question} helperText={errors.question?.message}
+                      InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                          <>
+                            {isFetchingQuestions ? <CircularProgress size={20} /> : null}
+                            {params.InputProps.endAdornment}
+                          </>
+                        ),
+                      }}
+                    />
+                  )}
+                />
+              )}
+            />
 
-          <FormControlLabel
-            control={<Switch checked={other} onChange={(e) => setOther(e.target.checked)} color="primary" />}
-            label="Outros"
-          />
+            <Controller
+              name="other"
+              control={control}
+              render={({ field }) => (
+                <FormControlLabel
+                  control={<Switch {...field} checked={field.value} color="primary" />}
+                  label="Outros"
+                />
+              )}
+            />
 
-          <Box sx={{ display: "flex", gap: 2, justifyContent: "center" }}>
-            <Button
-              variant="contained"
-              sx={{
-                backgroundColor: "#D32F2F",
-                color: "#FFF",
-                fontWeight: "bold",
-                width: "11rem",
-                padding: "10px",
-                borderRadius: "8px",
-                "&:hover": { backgroundColor: "#B71C1C" },
-              }}
-              onClick={handleCancel}
-            >
-              Cancelar
-            </Button>
-            <Button
-              variant="contained"
-              sx={{
-                background: "linear-gradient(135deg, #7E57C2, #5E3BEE)",
-                color: "#FFF",
-                fontWeight: "bold",
-                width: "11rem",
-                padding: "10px",
-                borderRadius: "8px",
-                "&:hover": { background: "linear-gradient(135deg, #5E3BEE, #7E57C2)" },
-              }}
-              onClick={handleSubmit}
-              disabled={loading}
-            >
-              {loading ? <CircularProgress size={20} sx={{ color: "#FFF" }} /> : "Salvar"}
-            </Button>
+            <Box sx={{ display: "flex", gap: 2, justifyContent: "center" }}>
+              <Button
+                variant="contained"
+                sx={{ backgroundColor: "#D32F2F", color: "#FFF", fontWeight: "bold", width: "11rem", padding: "10px", borderRadius: "8px", "&:hover": { backgroundColor: "#B71C1C" } }}
+                onClick={() => router.push("/home/answers")}
+              >
+                Cancelar
+              </Button>
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{ background: "linear-gradient(135deg, #7E57C2, #5E3BEE)", color: "#FFF", fontWeight: "bold", width: "11rem", padding: "10px", borderRadius: "8px", "&:hover": { background: "linear-gradient(135deg, #5E3BEE, #7E57C2)" } }}
+                disabled={createMutation.isLoading}
+              >
+                {createMutation.isLoading ? <CircularProgress size={20} sx={{ color: "#FFF" }} /> : "Salvar"}
+              </Button>
+            </Box>
           </Box>
-        </Box>
+        </form>
       </Paper>
     </Box>
   );
