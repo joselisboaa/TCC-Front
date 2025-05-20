23a3459efa8380cff5cf7dd34f881d40c3d08a05
joselisboaa <joselisboaa05@gmail.com>
2025-05-20 17:32:48 -0300

    tests: make tests evirontment

--- next.config.ts
+++ next.config.ts
@@ -6,4 +6,11 @@ const nextConfig: NextConfig = {
     },
 };
 
+const isTest = process.env.NEXT_PUBLIC_BACKEND_URL?.includes('8001');
+
+module.exports = {
+  distDir: isTest ? '.next_test' : '.next',
+};
+
+
 export default nextConfig;
